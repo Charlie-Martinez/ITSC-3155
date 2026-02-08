8a24ee4a67b503396abc877bb3925f30b4e9b66c
Charlie-Martinez <charliemartinez-rodriguez@outlook.com>
2026-02-08 18:22:21 -0500

    feat: implement transaction_result function

--- main.py
+++ main.py
@@ -66,6 +66,13 @@ class SandwichMachine:
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
+        if coins < cost:
+            print("Sorry, you don't have enough money to pay for this.")
+            return False
+        else:
+            if (cost - coins) > 0:
+                print(f"Here is ${cost - coins} in change.")
+            return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
         """Deduct the required ingredients from the resources.
