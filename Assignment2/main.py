f4fa538ae4174f34ed65946709134e237a82eabb
Charlie-Martinez <charliemartinez-rodriguez@outlook.com>
2026-02-13 22:16:40 -0500

    fix: updated main.py with specific imports and changed namings to match skeleton provided

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -6,8 +6,8 @@ from cashier import Cashier
 # Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
-sandwich_maker_instance = #####
-cashier_instance = ######
+sandwich_maker_instance = SandwichMaker(resources)
+cashier_instance = Cashier()
 
 
 
@@ -20,20 +20,20 @@ def main():
             print("Machine has been powered off.")
             break
         elif sandwich_size == "report":
-            print(f"Bread: {machine.machine_resources['bread']} slice(s)")
-            print(f"Ham: {machine.machine_resources['ham']} slice(s)")
-            print(f"Cheese: {machine.machine_resources['cheese']} ounce(s)")
+            print(f"Bread: {sandwich_maker_instance.machine_resources['bread']} slice(s)")
+            print(f"Ham: {sandwich_maker_instance.machine_resources['ham']} slice(s)")
+            print(f"Cheese: {sandwich_maker_instance.machine_resources['cheese']} ounce(s)")
         elif sandwich_size in ["small", "medium", "large"]:
             ingredients_needed = recipes[sandwich_size]["ingredients"]
             cost = recipes[sandwich_size]["cost"]
 
-            if not machine.check_resources(ingredients_needed):
+            if not sandwich_maker_instance.check_resources(ingredients_needed):
                 print(f"Sorry there is not enough ingredients.")
             else:
-                coins = machine.process_coins()
+                coins = cashier_instance.process_coins()
 
-                if machine.transaction_result(coins, cost):
-                    machine.make_sandwich(sandwich_size, ingredients_needed)
+                if cashier_instance.transaction_result(coins, cost):
+                    sandwich_maker_instance.make_sandwich(sandwich_size, ingredients_needed)
                     print(f"{sandwich_size} sandwich is ready. Bon appetit!")
         else:
             print("Invalid input, try again.")
