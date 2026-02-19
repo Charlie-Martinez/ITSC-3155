51f2a5fada34aa632e47f7efe66b70b15ab6b837
Charlie-Martinez <charliemartinez-rodriguez@outlook.com>
2026-02-18 23:44:08 -0500

    q1 requirements: defined math operation methods without implementation

---
+++ PyUnitTestingActivity/calculator.py
@@ -0,0 +1,17 @@
+class Calculator:
+
+    @staticmethod
+    def add(a, b):
+        pass
+
+    @staticmethod
+    def subtract(a, b):
+        pass
+
+    @staticmethod
+    def multiply(a, b):
+        pass
+
+    @staticmethod
+    def divide(a, b):
+        pass
