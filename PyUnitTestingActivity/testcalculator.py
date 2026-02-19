bb95e2914a3e1a174096b74b28bc28defbd20334
Charlie-Martinez <charliemartinez-rodriguez@outlook.com>
2026-02-19 00:09:49 -0500

    q4: implemented remaining test cases and assertations

--- PyUnitTestingActivity/testcalculator.py
+++ PyUnitTestingActivity/testcalculator.py
@@ -6,6 +6,19 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(Calculator.add(3, 2), 5)
+        self.assertEqual(Calculator.add(-1, 2), 1)
+
+    def test_subtract(self):
+        self.assertEqual(Calculator.subtract(3, 2), 1)
+        self.assertEqual(Calculator.subtract(-1, 2), -3)
+
+    def test_multiply(self):
+        self.assertEqual(Calculator.multiply(3, 2), 6)
+        self.assertEqual(Calculator.multiply(-1, 2), -2)
+
+    def test_divide(self):
+        self.assertEqual(Calculator.divide(3, 2), 1.5)
+        self.assertEqual(Calculator.divide(-1, 2), -0.5)
 
 
 if __name__ == '__main__':
